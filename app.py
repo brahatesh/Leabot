2ae7b972e6d7377676b922c02a311242575bf81c
Brahatesh Vasantha <brahatesh@gmail.com>
2023-01-14 15:04:08 +0530

    Added joke module and made code modular

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import sys
 import re
 
 from dotenv import load_dotenv
-import pyjokes
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 sys.path.append(os.path.abspath("./sql"))
+sys.path.append(os.path.abspath("./commands"))
 from employee.employee_update import update as employee_upd
+import commands
 
 load_dotenv()
 
@@ -18,16 +19,22 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
 logging.basicConfig(filename=".log", encoding="utf-8", level=logging.DEBUG)
-logger = logging.getLogger(__name__)    
+logger = logging.getLogger(__name__)
+
+form_progress = {}
 
 @app.event("message")
 def handle_message_events(message, logger, say):
     text = message["text"]
-    channel_type = message["channel_type"]
-    if channel_type != "im":
+    channel = message['channel']
+    if message["channel_type"] != "im":
         return
-    dm_channel = message["channel"]
     user_id = message["user"]
+
+    if(user_id in form_progress):
+        rep = form_progress[user_id].exec(message,logger)
+        say(text=rep, channel=message['channel'])
+        return
 
     if(text[0]!="."): 
         logger.info(f"Invalid command recieved < {message} > by {user_id}")
@@ -37,15 +44,16 @@ def handle_message_events(message, logger, say):
     command = text[1:].lower()
     match command:
         case "joke":
-            joke = pyjokes.get_joke()
-            logger.info(f"Sent joke < {joke} > to user {user_id} in channel {dm_channel} with channel_type {channel_type}")
-            say(text=joke, channel=dm_channel)
+            # joke = pyjokes.get_joke()
+            ret = commands.joke.make_obj().exec(message,logger)
+            # logger.info(f"Sent joke < {ret} > to user {user_id} in channel {dm_channel} with channel_type {channel_type}")
+            say(text=ret, channel=channel)
         
         case "update":
             employee_upd(user_id,4)
 
         case default:
-            say(text="Invalid command", channel=dm_channel)
+            say(text="Invalid command", channel=channel)
 
 
 def main():
---
+++ commands/__init__.py
@@ -0,0 +1,1 @@
+import joke_module as joke
