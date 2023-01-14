235d5022578fec299bab61b32a38a485bcd6d695
Harshitgoyal1402 <harshit_g@ch.iitr.ac.in>
2023-01-14 15:32:12 +0530

    Updated manager_side

--- sql/manager_side/insert.py
+++ sql/manager_side/insert.py
@@ -1,7 +1,7 @@
 import sqlite3
-conn = sqlite3.connect("./sql/manager_side/sqlite.db")
 
 def insert(ref_id,employee_id,type,no_of_leaves,reason):
+    conn = sqlite3.connect("./sql/manager_side/sqlite.db")
     params = (ref_id,employee_id,type,no_of_leaves,reason)
     conn.execute( f'''
     insert into manager (ref_id,employee_id,type,no_of_leaves,reason) VALUES
@@ -10,5 +10,3 @@ def insert(ref_id,employee_id,type,no_of_leaves,reason):
     conn.commit()
     conn.close()
 
-
-insert("123","U04JMH6NV7Y","sick",2,"got fever")
