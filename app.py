533f66d004842a032cfb0c23aad843abc01c6d74
Brahatesh Vasantha <brahatesh@gmail.com>
2023-01-14 11:58:41 +0530

    Changed app.py to command based

--- app.py
+++ app.py
@@ -18,28 +18,35 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
 logging.basicConfig(filename=".log", encoding="utf-8", level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-@app.message(re.compile(f"^[h|H][i|I]$"))
-def show_random_joke(message, say):
-    channel_type = message["channel_type"]
-    if channel_type != "im":
-        return
-    
-    dm_channel = message["channel"]
-    user_id = message["user"]
-
-    joke = pyjokes.get_joke()
-    logger.info(f"Sent joke < {joke} > to user {user_id} in channel {dm_channel} with channel_type {channel_type}")
-
-    employee_upd(user_id,4)
-
-    say(text=joke, channel=dm_channel)
+logger = logging.getLogger(__name__)    
 
 @app.event("message")
 def handle_message_events(message, logger, say):
-    logger.info(f"Invalid message recieved: {message}")
-    say(text="Invalid command", channel=message["channel"])
+    text = message["text"]
+    channel_type = message["channel_type"]
+    if channel_type != "im":
+        return
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    if(text[0]!="."): 
+        logger.info(f"Invalid command recieved < {message} > by {user_id}")
+        say(text="Invalid command", channel=message["channel"])
+        return
+
+    command = text[1:].lower()
+    match command:
+        case "joke":
+            joke = pyjokes.get_joke()
+            logger.info(f"Sent joke < {joke} > to user {user_id} in channel {dm_channel} with channel_type {channel_type}")
+            say(text=joke, channel=dm_channel)
+        
+        case "update":
+            employee_upd(user_id,4)
+
+        case default:
+            say(text="Invalid command", channel=dm_channel)
+
 
 def main():
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
